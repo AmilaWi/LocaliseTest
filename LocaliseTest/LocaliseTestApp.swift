609161253cb1d8f64bc4d42fc34d7c098e634eda
Amila Wijayarathna <amila.wijayarathna@pickme.lk>
2025-10-08 11:24:11 +0530

    Initial Commit

---
+++ LocaliseTest/LocaliseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaliseTestApp.swift
+//  LocaliseTest
+//
+//  Created by Amila on 2025-10-08.
+//
+
+import SwiftUI
+
+@main
+struct LocaliseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
